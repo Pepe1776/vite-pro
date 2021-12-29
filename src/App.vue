141991703b23eede2cfbaedefc245b7dee0f939b
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2021-12-28 21:33:47 -0700

    startin gto build

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue'
-
 let counter = ref(0)
 
 setInterval(() => {
@@ -9,12 +8,13 @@ setInterval(() => {
 </script>
 
 <template>
-  <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
+  <div id="container">
+    <header class="bg-black shadow" v-if="$route.meta.title">
       <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <h1
+          id="title"
           @click="counter = 0"
-          class="text-3xl font-bold leading-tight text-gray-900"
+          class="text-center text-3xl font-bold leading-tight text-gray-300"
         >
           {{ $route.meta.title }} / {{ counter }}
         </h1>
@@ -25,3 +25,12 @@ setInterval(() => {
     </main>
   </div>
 </template>
+<style scoped>
+@font-face {
+  font-family: 'Alien';
+  src: url('./assets/Sprykski.otf');
+}
+h1 {
+  font-family: 'Alien';
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,66 +3,33 @@ import ButtonRepo from '@/components/ButtonRepo.vue'
 </script>
 
 <template>
-  <div class="bg-gray-50" id="cont">
-    <div
-      class="
-        max-w-screen-xl
-        px-4
-        py-12
-        mx-auto
-        sm:px-6
-        lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between
-      "
-    >
-      <h2
-        class="
-          text-3xl
-          font-extrabold
-          leading-9
-          tracking-tight
-          text-gray-900
-          sm:text-4xl sm:leading-10
-        "
-      >
-        Full Stack Devolopment
-        <br />
-        <span class="text-lime-600">Vite + Vue 3 + Tailwind CSS + Node + Firebase</span>
-      </h2>
-      <div class="flex mt-8 lg:flex-shrink-0 lg:mt-0">
-        <div class="inline-flex rounded-md shadow">
-          <router-link
-            to="/about"
-            class="
-              inline-flex
-              items-center
-              justify-center
-              px-5
-              py-3
-              text-base
-              font-medium
-              leading-6
-              text-white
-              transition
-              duration-150
-              ease-in-out
-              bg-lime-600
-              border border-transparent
-              rounded-md
-              hover:bg-green-700
-              focus:outline-none
-            "
-            >More Info</router-link
-          >
-        </div>
-        <ButtonRepo />
-      </div>
+  <div id="cont">
+    <h2 class="text-center text-7xl font-bold leading-tight animate-pulse">Nterface</h2>
+    <h4 class="text-center text-4xl">Design => Deployment</h4>
+    <div id="p" class=" text-center text-2xl shadow-white-500">
+      Full stack web devolopment using the Vue3 framework and Vite <br /> to integrate
+      custom tailored systems for each individual application
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
 #cont {
-  background-image: url('../assets/wht2.jpg');
-  background-size: 150rem;
+  height: 1000px;
+  background-image: url('../assets/mac.jpg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+h2 {
+  color: #133eff;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+h4 {
+  color: #98c1d9;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+#p {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  color: #e0fbfc;
 }
 </style>
